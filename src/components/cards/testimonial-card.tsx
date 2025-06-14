01e4e5ac43150739ebb4c662f64b568f53eea396
irefransdotdev <irefrans.dev@gmail.com>
2025-06-14 18:39:01 +0800

    Update testimonial card padding

--- src/components/cards/testimonial-card.tsx
+++ src/components/cards/testimonial-card.tsx
@@ -1,5 +1,5 @@
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
-import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import { Card, CardDescription, CardHeader, CardTitle } from "../ui/card";
 
 interface TestimonialCardProps {
   testimonial: {
@@ -14,9 +14,9 @@ interface TestimonialCardProps {
 const TestimonialCard = ({ testimonial }: TestimonialCardProps) => {
   const { firstName, lastName, profession, title, message } = testimonial;
   return (
-    <article className="w-full h-full cursor-pointer  hover:outline hover:outline-secondary rounded-lg">
-      <Card className="border-none shadow-none bg-white flex">
-        <CardHeader className="font-bold text-xl">
+    <article className="w-full cursor-pointer  hover:outline hover:outline-secondary rounded-lg">
+      <Card className="border-none shadow-none bg-white flex flex-col justify-center">
+        <CardHeader className="pb-0">
           <CardTitle className="text-xl">
             <div className="flex flex-row gap-2 items-center">
               <Avatar>
@@ -24,23 +24,25 @@ const TestimonialCard = ({ testimonial }: TestimonialCardProps) => {
                   src="https://github.com/shadcn.png"
                   alt="@shadcn"
                 />
-                <AvatarFallback>
+                <AvatarFallback className="text-sm">
                   {`${firstName?.[0]} ${lastName?.[0]}`}
                 </AvatarFallback>
               </Avatar>
               <div className="flex flex-col">
-                <h2 className="text-start font-bold text-primary">{`${firstName} ${lastName}`}</h2>
-                <p className="text-secondary text-start font-normal text-sm">
+                <h2 className="text-start font-bold text-primary text-lg">{`${firstName} ${lastName}`}</h2>
+                <p className="text-secondary text-start font-normal text-sm mt-[-5px]">
                   {profession}
                 </p>
               </div>
             </div>
           </CardTitle>
+          <CardDescription>
+            <h2 className="font-bold text-md text-start text-primary text-lg">
+              {title}
+            </h2>
+            <p className="text-start text-secondary">{message}</p>
+          </CardDescription>
         </CardHeader>
-        <CardContent>
-          <h2 className="font-bold text-md text-start text-primary">{title}</h2>
-          <p className="text-start text-secondary">{message}</p>
-        </CardContent>
       </Card>
     </article>
   );
