c94e55765f3ad87f4fd3174d8046cc4385a7a23b
irefransdotdev <irefrans.dev@gmail.com>
2025-06-14 23:38:42 +0800

    Animate animation on header link

--- src/components/sections/header.tsx
+++ src/components/sections/header.tsx
@@ -1,12 +1,29 @@
+"use client";
 import Link from "next/link";
+import { useMotion } from "../providers/scroll/MotionProvider";
+import { useRouter } from "next/navigation";
 
 const Header = () => {
+  const motion = useMotion();
+  const router = useRouter();
   return (
     <header className="text-white" aria-label="Site Header">
       <div className="container mx-auto flex justify-between items-center">
-        <Link href="/" className="font-bold" title="Go to TopPickHubs homepage">
-          Top Picks Hub
-        </Link>
+        <motion.div
+          className="box"
+          transition={{ type: "spring", delay: 0.2 }}
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+        >
+          <Link
+            href="/"
+            className="font-bold"
+            title="Go to TopPickHubs homepage"
+            onClick={() => router.push("/")}
+          >
+            Top Picks Hub
+          </Link>
+        </motion.div>
         <nav aria-label="Main Navigation">
           {/* Navigation links go here in the future */}
         </nav>
