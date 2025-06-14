0f021d0abb31e486c2f9961d710721b234bdc0a1
irefransdotdev <irefrans.dev@gmail.com>
2025-06-14 22:23:05 +0800

    Add animations on latest blogs sections

--- src/components/cards/blog-card.tsx
+++ src/components/cards/blog-card.tsx
@@ -1,3 +1,4 @@
+import { useMotion } from "../providers/scroll/MotionProvider";
 import { Button } from "../ui/button";
 import {
   Card,
@@ -13,24 +14,29 @@ interface BlogCardProps {
 }
 
 const BlogCard = ({ title, description }: BlogCardProps) => {
+  const motion = useMotion();
   return (
-    <article className="w-full h-full cursor-pointer  hover:outline hover:outline-secondary rounded-lg">
-      <Card className="border-none shadow-none bg-card h-full">
-        <CardHeader className="font-bold text-xl">
-          <CardTitle className="text-xl">{title}</CardTitle>
-          <CardDescription className="text-start font-normal text-secondary">
-            {description}
-          </CardDescription>
-        </CardHeader>
-        <CardFooter>
-          <footer className="w-full">
-            <Button className="bg-button-primary w-full cursor-pointer hover:bg-button-primary/80">
-              Read More
-            </Button>
-          </footer>
-        </CardFooter>
-      </Card>
-    </article>
+    <motion.div whileHover={{ scale: 1.05 }}>
+      <article className="w-full h-full cursor-pointer  hover:outline hover:outline-secondary rounded-lg">
+        <Card className="border-none shadow-none bg-card h-full">
+          <CardHeader className="font-bold text-xl">
+            <CardTitle className="text-xl">{title}</CardTitle>
+            <CardDescription className="text-start font-normal text-secondary">
+              {description}
+            </CardDescription>
+          </CardHeader>
+          <CardFooter>
+            <footer className="w-full">
+              <motion.div whileTap={{ scale: 0.9 }}>
+                <Button className="bg-button-primary w-full cursor-pointer hover:bg-button-primary/80">
+                  Read More
+                </Button>
+              </motion.div>
+            </footer>
+          </CardFooter>
+        </Card>
+      </article>
+    </motion.div>
   );
 };
 
--- src/components/sections/latest-blogs.tsx
+++ src/components/sections/latest-blogs.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import BlogCard from "../cards/blog-card";
+import { useMotion } from "../providers/scroll/MotionProvider";
 
 const latestBlogs = [
   {
@@ -19,27 +22,50 @@ const latestBlogs = [
 ];
 
 const LatestBlogs = () => {
+  const motion = useMotion();
   return (
     <section className="text-gray-900 w-full py-5">
       <div className="px-4 gap-5 flex flex-col items-center text-center">
         <header>
-          <h2 className="text-xl text-primary font-bold">
-            Latest Product Reviews, Comparisons, and Expert Buying Guides
-          </h2>
-          <p className="text-secondary">
-            Stay updated with our latest product reviews, in-depth comparisons,
-            and helpful buying guides designed to make your online shopping
-            experience easier and smarter.
-          </p>
+          <motion.div
+            className="box"
+            transition={{ type: "spring" }}
+            initial={{ opacity: 0, scale: 0 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+          >
+            <h2 className="text-xl text-primary font-bold">
+              Latest Product Reviews, Comparisons, and Expert Buying Guides
+            </h2>
+          </motion.div>
+          <motion.div
+            className="box"
+            transition={{ type: "spring", delay: 0.1 }}
+            initial={{ opacity: 0, scale: 0 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+          >
+            <p className="text-secondary">
+              Stay updated with our latest product reviews, in-depth
+              comparisons, and helpful buying guides designed to make your
+              online shopping experience easier and smarter.
+            </p>
+          </motion.div>
         </header>
         <div className="flex flex-col gap-4 w-full">
           <div className="flex flex-col gap-4 w-full">
             {latestBlogs.map((latestBlog, index) => (
-              <BlogCard
+              <motion.div
+                className="box"
+                transition={{ type: "spring", delay: index * 0.1 }}
+                initial={{ opacity: 0, scale: 0 }}
+                whileInView={{ opacity: 1, scale: 1 }}
                 key={index}
-                title={latestBlog.title}
-                description={latestBlog.description}
-              />
+              >
+                <BlogCard
+                  key={index}
+                  title={latestBlog.title}
+                  description={latestBlog.description}
+                />
+              </motion.div>
             ))}
           </div>
         </div>
