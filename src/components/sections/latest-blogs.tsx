3203d7d58dc5d01749f1eef0db0b83be4f9ef351
irefransdotdev <irefrans.dev@gmail.com>
2025-06-14 22:38:48 +0800

    Update latest blogs card animation

--- src/components/sections/latest-blogs.tsx
+++ src/components/sections/latest-blogs.tsx
@@ -55,7 +55,10 @@ const LatestBlogs = () => {
             {latestBlogs.map((latestBlog, index) => (
               <motion.div
                 className="box"
-                transition={{ type: "spring", delay: index * 0.1 }}
+                transition={{
+                  type: "keyframes",
+                  delay: Number(((index + 1) * 0.1).toFixed(1)),
+                }}
                 initial={{ opacity: 0, scale: 0 }}
                 whileInView={{ opacity: 1, scale: 1 }}
                 key={index}
