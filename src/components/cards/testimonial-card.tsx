68076765ee1789b2f3497b0d5267f1b10d373ba3
irefransdotdev <irefrans.dev@gmail.com>
2025-06-22 15:21:53 +0800

    Add animation scale on testimonial card

--- src/components/cards/testimonial-card.tsx
+++ src/components/cards/testimonial-card.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useMotion } from "../providers/scroll/MotionProvider";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { Card, CardDescription, CardHeader, CardTitle } from "../ui/card";
 
@@ -15,38 +16,41 @@ interface TestimonialCardProps {
 
 const TestimonialCard = ({ testimonial }: TestimonialCardProps) => {
   const { firstName, lastName, profession, title, message } = testimonial;
+  const motion = useMotion();
   return (
-    <article className="w-full cursor-pointer  hover:outline hover:outline-secondary rounded-lg">
-      <Card className="border-none shadow-none bg-white flex flex-col justify-center">
-        <CardHeader className="pb-0">
-          <CardTitle className="text-xl">
-            <div className="flex flex-row gap-2 items-center">
-              <Avatar>
-                <AvatarImage
-                  src="https://github.com/shadcn.png"
-                  alt="@shadcn"
-                />
-                <AvatarFallback className="text-sm">
-                  {`${firstName?.[0]} ${lastName?.[0]}`}
-                </AvatarFallback>
-              </Avatar>
-              <div className="flex flex-col">
-                <h2 className="text-start font-bold text-primary text-lg">{`${firstName} ${lastName}`}</h2>
-                <p className="text-secondary text-start font-normal text-sm mt-[-5px]">
-                  {profession}
-                </p>
+    <motion.div whileHover={{ scale: 1.05 }}>
+      <article className="w-full cursor-pointer  hover:outline hover:outline-secondary rounded-lg">
+        <Card className="border-none shadow-none bg-white flex flex-col justify-center">
+          <CardHeader className="pb-0">
+            <CardTitle className="text-xl">
+              <div className="flex flex-row gap-2 items-center">
+                <Avatar>
+                  <AvatarImage
+                    src="https://github.com/shadcn.png"
+                    alt="@shadcn"
+                  />
+                  <AvatarFallback className="text-sm">
+                    {`${firstName?.[0]} ${lastName?.[0]}`}
+                  </AvatarFallback>
+                </Avatar>
+                <div className="flex flex-col">
+                  <h2 className="text-start font-bold text-primary text-lg">{`${firstName} ${lastName}`}</h2>
+                  <p className="text-secondary text-start font-normal text-sm mt-[-5px]">
+                    {profession}
+                  </p>
+                </div>
               </div>
-            </div>
-          </CardTitle>
-          <CardDescription>
-            <h2 className="font-bold text-md text-start text-primary text-lg">
-              {title}
-            </h2>
-            <p className="text-start text-secondary">{message}</p>
-          </CardDescription>
-        </CardHeader>
-      </Card>
-    </article>
+            </CardTitle>
+            <CardDescription>
+              <h2 className="font-bold text-md text-start text-primary text-lg">
+                {title}
+              </h2>
+              <p className="text-start text-secondary">{message}</p>
+            </CardDescription>
+          </CardHeader>
+        </Card>
+      </article>
+    </motion.div>
   );
 };
 
