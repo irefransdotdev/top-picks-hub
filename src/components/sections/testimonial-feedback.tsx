b4d8e466f7d260e1f80f824165312236fb6d823d
irefransdotdev <irefrans.dev@gmail.com>
2025-06-22 18:18:11 +0800

    Add desktop view testimonial feedback form

--- src/components/sections/testimonial-feedback.tsx
+++ src/components/sections/testimonial-feedback.tsx
@@ -6,36 +6,40 @@ import { useMotion } from "../providers/scroll/MotionProvider";
 const TestimonialFeedback = () => {
   const motion = useMotion();
   return (
-    <section className="text-gray-900 w-full py-5">
-      <div className="px-4 gap-5 flex flex-col items-center text-center">
-        <header>
-          <motion.div
-            className="box"
-            transition={{ type: "spring" }}
-            initial={{ opacity: 0, scale: 0 }}
-            whileInView={{ opacity: 1, scale: 1 }}
-            viewport={{ once: true }}
-          >
-            <h2 className="text-xl text-primary font-bold">
-              Share Your Experience with TopPicksHub!
-            </h2>
-          </motion.div>
-          <motion.div
-            className="box"
-            transition={{ type: "spring", delay: 0.1 }}
-            initial={{ opacity: 0, scale: 0 }}
-            whileInView={{ opacity: 1, scale: 1 }}
-            viewport={{ once: true }}
-          >
-            <p className="text-secondary">
-              We&#39;d love to hear how TopPicksHub helped you make smarter
-              buying decisions. Share your testimonial below:
-            </p>
-          </motion.div>
-        </header>
-        <div className="w-full">
+    <section className="w-full container mx-auto">
+      <div className="flex flex-col gap-5 lg:gap-10 text-center lg:text-start lg:flex-row">
+        <section className="order-2">
+          <article>
+            <motion.div
+              className="box"
+              transition={{ type: "spring" }}
+              initial={{ opacity: 0, scale: 0 }}
+              whileInView={{ opacity: 1, scale: 1 }}
+              viewport={{ once: true }}
+            >
+              <h2 className="text-xl text-primary font-bold">
+                Share Your Experience with TopPicksHub!
+              </h2>
+            </motion.div>
+            <motion.div
+              className="box"
+              transition={{ type: "spring", delay: 0.1 }}
+              initial={{ opacity: 0, scale: 0 }}
+              whileInView={{ opacity: 1, scale: 1 }}
+              viewport={{ once: true }}
+            >
+              <p className="text-secondary">
+                We&#39;d love to hear how TopPicksHub helped you make smarter
+                buying decisions. Your feedback helps us improve and assist more
+                users like you. Share your story or suggestions—every message
+                matters!
+              </p>
+            </motion.div>
+          </article>
+        </section>
+        <section className="basis-2xl order-1">
           <TestimonialForm />
-        </div>
+        </section>
       </div>
     </section>
   );
