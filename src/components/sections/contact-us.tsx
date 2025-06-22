c13a06e44b48dae1a419d5410a0956e0f307d89d
irefransdotdev <irefrans.dev@gmail.com>
2025-06-22 18:24:00 +0800

    Update contact us, testimonial feedback forms width

--- src/components/sections/contact-us.tsx
+++ src/components/sections/contact-us.tsx
@@ -8,8 +8,8 @@ const ContactUs = () => {
   return (
     <section className="w-full container mx-auto">
       <div className="flex flex-col gap-5 lg:gap-10 text-center lg:text-start lg:flex-row">
-        <article>
-          <header>
+        <section className="lg:basis-full">
+          <article>
             <motion.div
               className="box"
               transition={{ type: "spring" }}
@@ -21,25 +21,24 @@ const ContactUs = () => {
                 Contact Us - Get in Touch with TopPicksHub
               </h2>
             </motion.div>
-          </header>
+            <motion.div
+              className="box"
+              transition={{ type: "spring", delay: 0.1 }}
+              initial={{ opacity: 0, scale: 0 }}
+              whileInView={{ opacity: 1, scale: 1 }}
+              viewport={{ once: true }}
+            >
+              <p className="text-secondary">
+                We&#39;d love to hear from you! Whether you have questions,
+                suggestions, product recommendations, or partnership
+                inquiries—feel free to reach out. At TopPicksHub, your feedback
+                helps us improve our content and serve you better.
+              </p>
+            </motion.div>
+          </article>
+        </section>
+        <section className="w-full space-y-2 lg:space-y-4 lg:basis-full">
           <motion.div
-            className="box"
-            transition={{ type: "spring", delay: 0.1 }}
-            initial={{ opacity: 0, scale: 0 }}
-            whileInView={{ opacity: 1, scale: 1 }}
-            viewport={{ once: true }}
-          >
-            <p className="text-secondary">
-              We&#39;d love to hear from you! Whether you have questions,
-              suggestions, product recommendations, or partnership
-              inquiries—feel free to reach out. At TopPicksHub, your feedback
-              helps us improve our content and serve you better.
-            </p>
-          </motion.div>
-        </article>
-        <footer className="w-full space-y-2 lg:space-y-4">
-          <motion.div
-            className="box"
             transition={{ type: "spring", delay: 0.2 }}
             initial={{ opacity: 0, scale: 0 }}
             whileInView={{ opacity: 1, scale: 1 }}
@@ -52,7 +51,7 @@ const ContactUs = () => {
             </p>
           </motion.div>
           <ContactUsForm />
-        </footer>
+        </section>
       </div>
     </section>
   );
